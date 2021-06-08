ffb4717ff2f5c5177cad0e00a2cdc33649eb974f
haristauqir <haristauqir@gmail.com>
2021-06-08 15:15:56 +0100

    Catalog Service added

---
+++ src/Services/Catalog/Catalog.API/Startup.cs
@@ -0,0 +1,106 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+
+namespace Catalog.API
+{
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
+    {
+      Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      //services.AddControllers()
+      services.AddControllers().AddNewtonsoftJson(opt =>
+      {
+        opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+      })
+     .Services
+     .AddCustomMVC(Configuration)
+     .AddSwagger(Configuration);
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      //app.UseHttpsRedirection();
+
+      app.UseRouting();
+
+      app.UseAuthorization();
+
+      app.UseSwagger()
+            .UseSwaggerUI(c =>
+            {
+              c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog.API V1");
+            });
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
+
+  public static class CustomExtensionMethods
+  {
+
+    public static IServiceCollection AddCustomMVC(this IServiceCollection services, IConfiguration configuration)
+    {
+      services.AddCors(options =>
+      {
+        options.AddPolicy("CorsPolicy",
+                  builder => builder
+                  .SetIsOriginAllowed((host) => true)
+                  .AllowAnyMethod()
+                  .AllowAnyHeader()
+                  .AllowCredentials());
+      });
+
+      return services;
+    }
+
+    public static IServiceCollection AddSwagger(this IServiceCollection services, IConfiguration configuration)
+    {
+      services.AddSwaggerGen(options =>
+      {
+        options.SwaggerDoc("v1", new OpenApiInfo
+        {
+          Title = "Catalog.API",
+          Version = "v1",
+          Description = "The Catalog Microservice",
+          Contact = new OpenApiContact
+          {
+            Name = "M Haris",
+            Email = string.Empty,
+            Url = new Uri("https://github.com/haristauqir"),
+          }
+        });
+      });
+
+      return services;
+
+    }
+  }
+}
