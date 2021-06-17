f3c1dae86ec6c005f0798225ccd3620bc1c980c3
haristauqir <haristauqir@gmail.com>
2021-06-17 14:53:23 +0100

    Ordering Microservice With DDD/CQRS Part 1

---
+++ src/Services/Ordering/Ordering.API/Extensions/CustomStartupExtensionMethods.cs
@@ -0,0 +1,55 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace Ordering.API.Extensions
+{
+   public static class CustomStartupExtensionMethods
+  {
+
+    /*public static IServiceCollection AddCustomOptions(this IServiceCollection services, IConfiguration configuration)
+    {
+      services.Configure<BasketSetttings>(configuration);
+      return services;
+    }
+
+    public static IServiceCollection AddRedisCache(this IServiceCollection services, IConfiguration configuration)
+    {
+      return services.AddStackExchangeRedisCache(options =>
+      {
+          options.Configuration = configuration.GetValue<string>("CacheSettings:ConnectionString");
+      });
+    }*/
+
+    public static IServiceCollection AddCustomMVC(this IServiceCollection services, IConfiguration configuration)
+    {
+      services.AddCors(options =>
+      {
+        options.AddPolicy("CorsPolicy",
+                  builder => builder
+                  .SetIsOriginAllowed((host) => true)
+                  .AllowAnyMethod()
+                  .AllowAnyHeader()
+                  .AllowCredentials());
+      });
+
+      return services;
+    }
+
+    public static IServiceCollection AddSwagger(this IServiceCollection services, IConfiguration configuration)
+    {
+      services.AddSwaggerGen(options =>
+      {
+        options.SwaggerDoc("v1", new OpenApiInfo
+        {
+          Title = "Ordering.API",
+          Version = "v1",
+          Description = "The Ordering Microservice"
+        });
+      });
+
+      return services;
+
+    }
+  }
+}
